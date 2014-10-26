c2069a70881424679d3b55676d8588e964942f24
= <lge920904@naver.com>
2014-10-27 00:43:52 +0900

    돼라이새끼야

---
+++ SnapMovieV01/src/ac/mju/snapmovie/VideoFilterActivity.java
@@ -0,0 +1,35 @@
+package ac.mju.snapmovie;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.VideoView;
+
+public class VideoFilterActivity extends Activity implements OnClickListener {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_video_filter);
+
+		Button prevButton = (Button) findViewById(R.id.btn_videoFilter_prev);
+		Button nextButton = (Button) findViewById(R.id.btn_videoFilter_next);
+		VideoView FilterdVideo = (VideoView) findViewById(R.id.videoview_filtered_video);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.btn_videoFilter_prev:
+			startActivity(new Intent(this, MainActivity.class));
+			break;
+		case R.id.btn_videoFilter_next:
+			startActivity(new Intent(this, MainActivity.class));
+			break;
+		}
+	}
+}
