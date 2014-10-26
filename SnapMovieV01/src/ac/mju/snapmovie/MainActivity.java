ed9940fd2b6f6aeaa0f6866acb896b78037ef4a0
Kyungman <nuna13@naver.com>
2014-10-26 21:25:34 +0900

    init

---
+++ SnapMovieV01/src/ac/mju/snapmovie/MainActivity.java
@@ -0,0 +1,50 @@
+package ac.mju.snapmovie;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+	private Button testButton;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		testButton = (Button) findViewById(R.id.button_test);
+		testButton.setOnClickListener(this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up b`utton, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	public void onClick(View v) {
+		if (v.getId() == R.id.button_test) {
+			Intent intent = new Intent(this, VideoActivity.class);
+			// Intent intent = new Intent(this, RecordActivity.class);
+			// Intent intent = new Intent(this, ShootVideoActivity.class);
+			startActivity(intent);
+		}
+	}
+}
